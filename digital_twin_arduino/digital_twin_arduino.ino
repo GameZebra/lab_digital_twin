e04d84d5ff5f4b1ec0fb70120ec541ba09d9ee68
GameZebra <mr.xmitkotox@gmail.com>
2024-04-04 19:56:48 +0300

    arduino, all buttons added

--- digital_twin_arduino/digital_twin_arduino.ino
+++ digital_twin_arduino/digital_twin_arduino.ino
@@ -1,6 +1,9 @@
-const int slider = A3;
-const int button1 = 7;
-const int button2 = 8;
+const int slider = 7;
+const int button1 = 8;
+const int button2 = 9;
+const int button3 = 10;
+const int button4 = 11;
+const int button5 = 12;
 
 void setup() {
   Serial.begin(9600);
@@ -11,5 +14,11 @@ void loop() {
   Serial.print(",");
   Serial.print(digitalRead(button1));
   Serial.print(",");
-  Serial.println(digitalRead(button2));
+  Serial.print(digitalRead(button2));
+  Serial.print(",");
+  Serial.print(digitalRead(button3));
+  Serial.print(",");
+  Serial.print(digitalRead(button4));
+  Serial.print(",");
+  Serial.println(digitalRead(button5));
 }
